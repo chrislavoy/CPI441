332dd9a60febf353f91581bd3bb2d78df1f2fc1d
CLavoy <chrislavoy@gmail.com>
2015-03-17 10:58:58 -0700

    Updating Time of Day Script

--- Assets/Scripts/TimeOfDay.cs
+++ Assets/Scripts/TimeOfDay.cs
@@ -1,21 +1,51 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System;
 using System.Collections;
 
-public class TimeOfDay : MonoBehaviour 
+public class TimeOfDay : MonoBehaviour
 {
-	public bool useCurrentTime;
-	public bool dayAndNightChanges;
-
-	// Use this for initialization
+	public bool lightChanges;
+	public bool timeChanges;
+	public float dayLength;
+	
+	public float currentTime;
+	public int currentHour;
+	private Light sun;
+	
 	void Start () 
 	{
-	
+		sun = GetComponent<Light>();
+		currentTime = (transform.rotation.x / 360.0f) * 24.0f + (;
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
+		if (timeChanges) 
+		{
+			currentTime += Time.deltaTime;
+			//currentTime = currentTime % dayLength;
 
+			currentHour = (int)(Mathf.Ceil((currentTime / dayLength) * 24.0f) % 24.0f);
+			//currentHour = (int) currentTime / (int)dayLength;
+			Mathf.Clamp(currentHour, 0, 24);
+		}
+
+		if (lightChanges) 
+		{
+			transform.Rotate(Vector3.right * ((Time.deltaTime / dayLength) * 360.0f), Space.Self);
+
+			if (currentHour >= 7 && currentHour <= 18) 
+			{
+				sun.intensity = Mathf.Lerp(0.0f, 1.0f, Time.time);
+				RenderSettings.fog = true;
+				RenderSettings.ambientIntensity = Mathf.Lerp(0.5f, 1.0f, Time.time);
+			}
+			else
+			{
+				sun.intensity = Mathf.Lerp(1.0f, 0.0f, Time.time);
+				RenderSettings.fog = false;
+				RenderSettings.ambientIntensity = Mathf.Lerp(1.0f, 0.5f, Time.time);
+			}
+		}
 	}
 }
